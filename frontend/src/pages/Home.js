93463fe3d0c62e0b65d9eddcb758f22861f4882b
cookie5321 <25889269+cookie5321@users.noreply.github.com>
2025-03-07 16:09:08 +0900

    wip

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,19 @@
+import { useEffect, useState } from 'react'
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('http://localhost:4001/api/workouts')
+            const json = await response.body()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+    }, [])
+
     return (
         <div className="home">
             <h2>Home!</h2>
