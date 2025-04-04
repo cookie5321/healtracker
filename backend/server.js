da06f9744d58540a379b6108f4491803bf3a38d7
cookie5321 <25889269+cookie5321@users.noreply.github.com>
2025-04-04 14:44:32 +0900

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+const workoutRouter = require('./routes/workouts');
+
+const app = express();
+
+// logging middleware
+app.use((req, res, next) => {
+    console.log(req.method, req.path);
+    next();
+})
+
+app.use('/api/workouts', workoutRouter);
+
+app.listen(process.env.PORT, () => {
+    console.log("listening on port", process.env.PORT);
+});
