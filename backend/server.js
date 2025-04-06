1328f02f5a3e7dae90bf9bac9b26a52b89e684b9
cookie5321 <25889269+cookie5321@users.noreply.github.com>
2025-04-07 04:09:27 +0900

    backend API

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,29 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const workoutRouter = require('./routes/workouts');
+const express = require('express')
+const workoutRouter = require('./routes/workouts')
+const mongoose = require('mongoose')
 
-const app = express();
+const app = express()
 
+app.use(express.json()) // attach the request body JSON to req.body
 // logging middleware
 app.use((req, res, next) => {
-    console.log(req.method, req.path);
-    next();
+    console.log(req.method, req.path)
+    next()
 })
 
-app.use('/api/workouts', workoutRouter);
+app.use('/api/workouts', workoutRouter)
 
-app.listen(process.env.PORT, () => {
-    console.log("listening on port", process.env.PORT);
-});
+// connect to the DB using mongoose
+mongoose.connect(process.env.MONGO_URI) // async; return a promise
+    .then(() => {
+        console.log("connected to the DB")
+        // start listening ONLY after the DB is connected
+        app.listen(process.env.PORT, () => {
+            console.log("listening on port", process.env.PORT)
+        })
+    })
+    .catch((err) => {
+        console.log("ERROR:", err)
+    })
